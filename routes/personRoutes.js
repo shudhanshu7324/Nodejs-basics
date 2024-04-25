5dd843cdeec8b9c3643414c51a6974c05bc6ae8f
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-04-25 18:46:21 +0530

    delete api

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -68,5 +68,37 @@ router.post("/", async (req, res) => {
     }
   })
 
+  // delete api
+  // router.delete('/:id',async (req,res)=>{
+  //   try {
+  //     const personId = req.params.id;
+  //     const response = await Person.findByIdAndRemove(personId);
+  //     if(!response){
+  //       return res.status(404).json({error: 'Person not found'})
+  //     }
+  //     console.log("Data removed");
+  //     res.status(200).json(response)
+  //   } catch (error) {
+  //     console.log("error");
+  //     res.status(500).json({error: 'Internal server error'})
+  //   }
+  // })
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const personId = req.params.id;
+      const response = await Person.findByIdAndDelete(personId);
+      if (!response) {
+        return res.status(404).json({ error: 'Person not found' });
+      }
+      console.log("Data removed:", response);
+      res.status(200).json(response);
+    } catch (error) {
+      console.error("Error during person deletion:", error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+
 
   module.exports = router;  
